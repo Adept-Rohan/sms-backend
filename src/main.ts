09b0de2a629e175bcf0b8beba0342bfc83a9cb3e
Adept-Rohan <rohan1d20@gmail.com>
2024-04-08 14:28:24 +0545

    fix(api): enabled cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('SMS API')
     .setDescription('API for student management software')
@@ -12,6 +13,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(3030);
 }
 bootstrap();
