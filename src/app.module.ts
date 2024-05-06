0a93f985977ea9435d247f12f0db3f4e207b0154
Adept-Rohan <rohan1d20@gmail.com>
2024-05-06 23:58:04 +0545

    configuration of main and app module updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { InjectKnex } from './knex/knex.decorator';
 import { Knex } from 'knex';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { FloorModule } from './floor/floor.module';
+import { RoomModule } from './room/room.module';
 
 @Module({
-  imports: [UserModule, KnexModule, ConfigModule, AuthModule],
+  imports: [UserModule, KnexModule, ConfigModule, AuthModule, FloorModule, RoomModule],
 })
 export class AppModule implements OnModuleInit {
   constructor(@InjectKnex private knex: Knex) {}
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const port = process.env.port || 3030;
+
   const config = new DocumentBuilder()
-    .setTitle('SMS API')
-    .setDescription('API for student management software')
+    .setTitle('HMS API')
+    .setDescription('API for hostel management software')
     .setVersion('1.0')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3030);
+  await app.listen(port);
+
+  Logger.log(`Application running in http://localhost:${port}/api`);
 }
 bootstrap();
